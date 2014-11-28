9e808e22576d34fbcb70cf4e5a0c86dba23682c7
Quenio dos Santos <quenio@me.com>
2014-11-28 05:46:11 -0200

    * Binding the folder path used by the RootFolderProcessingCommand with the corresponding text field.

--- src/checkspace/commands/MainController.java
+++ src/checkspace/commands/MainController.java
@@ -3,24 +3,33 @@ package checkspace.commands;
 import checkspace.gui.IO;
 import checkspace.processing.RootFolderProcessor;
 import checkspace.reports.UsageReport;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class MainController
 {
   private final IO io = new IO();
+  private final StringProperty folderPathProperty = new SimpleStringProperty();
   private final RootFolderProcessor rootFolderProcessor = new RootFolderProcessor(io);
   private final UsageReport usageReport = new UsageReport(io);
   private final RootFolderProcessingCommand rootFolderProcessingCommand =
-    new RootFolderProcessingCommand(io, rootFolderProcessor, usageReport);
+    new RootFolderProcessingCommand(io, folderPathProperty, rootFolderProcessor, usageReport);
 
   @FXML
   private TextField folderPathTextField;
 
   @FXML
+  public void initialize()
+  {
+    folderPathProperty.bindBidirectional(folderPathTextField.textProperty());
+    rootFolderProcessingCommand.initialize();
+  }
+
+  @FXML
   public void changeFolder()
   {
-    final String folderPath = folderPathTextField.getText();
-    rootFolderProcessingCommand.execute(folderPath);
+    rootFolderProcessingCommand.execute();
   }
 }
