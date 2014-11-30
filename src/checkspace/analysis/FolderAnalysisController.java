a1c2f927e5c4292ea48e0e7dfec77ae8a287e0e9
Quenio dos Santos <quenio@me.com>
2014-11-30 16:59:49 -0200

    Introducing the ability to cancel the analysis.

--- src/checkspace/analysis/FolderAnalysisController.java
+++ src/checkspace/analysis/FolderAnalysisController.java
@@ -6,14 +6,18 @@ import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 
+import static javafx.beans.binding.Bindings.not;
+
 public class FolderAnalysisController extends Controller implements EventHandler<WorkerStateEvent>
 {
-  @FXML
-  private TextField folderPathTextField;
+  @FXML private TextField folderPathTextField;
+  @FXML private Button startButton;
+  @FXML private Button cancelButton;
 
   private final FolderAnalysis folderAnalysis;
   private final FolderAnalysisService folderAnalysisService;
@@ -34,22 +38,42 @@ public class FolderAnalysisController extends Controller implements EventHandler
 
     folderAnalysis.bindFolderPathToProperty(folderPathTextField.textProperty());
     folderPathTextField.setText(System.getProperty("user.home"));
+
+    startButton.managedProperty().bind(startButton.visibleProperty());
+    cancelButton.visibleProperty().bind(not(startButton.visibleProperty()));
+    cancelButton.managedProperty().bind(not(startButton.visibleProperty()));
+
+    folderAnalysisService.setOnSucceeded(this);
+    folderAnalysisService.setOnCancelled(this);
+    folderAnalysisService.setOnFailed(this);
   }
 
   @FXML
-  public void analyzeFolder(final ActionEvent actionEvent)
+  public void startFolderAnalysis(final ActionEvent actionEvent)
   {
-    folderAnalysisService.setOnSucceeded(this);
+    startButton.setVisible(false);
     folderAnalysisService.restart();
+  }
+
+  @FXML
+  public void cancelFolderAnalysis(final ActionEvent actionEvent)
+  {
+    folderAnalysisService.cancel();
   }
 
   @Override
   public void handle(final WorkerStateEvent event)
   {
     final Worker worker = event.getSource();
-    if (worker.getState() == Worker.State.SUCCEEDED)
+    switch (worker.getState())
     {
-//      final FolderAnalysis folderAnalysis = (FolderAnalysis) worker.getValue();
+      case SUCCEEDED:
+      case CANCELLED:
+      case FAILED:
+        startButton.setVisible(true);
+        break;
+      default:
+        // no-op
     }
   }
 
